5fb75b2ba5a9294a719a3d68e9998727a87451ae
kakomarina <ninakako@hotmail.com>
2019-03-29 08:00:54 -0300

    Started exercise 6

---
+++ src/se/hh/aoop/assignment1/exercise6/MessageStack.java
@@ -0,0 +1,48 @@
+package se.hh.aoop.assignment1.exercise6;
+
+import java.util.Stack;
+import se.hh.aoop.assignment1.exercise5.Message;
+
+public class MessageStack {
+
+    private Stack<Message> stack;
+    private int size;
+
+    public MessageStack(){
+        this.stack = new Stack<Message>();
+        this.size = 0;
+    }
+
+    public void push(Message m){
+        this.size++;
+        this.stack.push(m);
+    }
+
+    public Message pop(){
+        this.size--;
+        return this.stack.pop();
+    }
+
+    public void push (int n, Message[] m){
+        for(int i = 0; i < n; i++){
+            this.stack.push(m[i]);
+        }
+        this.size = this.size + n;
+    }
+
+    public Message[] pop(int n){
+       Message[] m = new Message[n];
+
+        for(int i = n - 1; i >= 0; i--) {
+            m[i] = this.stack.pop();
+        }
+
+        this.size = this.size - n;
+
+        return m;
+    }
+
+    public int size(){
+        return this.size;
+    }
+}
