f904acc82d4aa153cbaa7d2b1847f8be26ce76c0
l0renor <leon.lukas11@web.de>
2019-05-02 17:38:02 +0200

    assignemnt3 interfaces

---
+++ src/se/hh/aoop/src/assignment3/Tree.java
@@ -0,0 +1,36 @@
+package se.hh.aoop.src.assignment3;
+
+import java.lang.reflect.InvocationTargetException;
+
+@Element(name = "node")
+public class Tree<T> {
+
+    private Tree<T>[] children = null;
+    private T value;
+
+    public Tree(T v, Tree<T>[] trees) {
+        children = trees;
+        value = v;
+    }
+
+    public Tree(T v) {
+        value = v;
+    }
+
+    @SubElements(name = "subnodes")
+    public Tree<T>[] getChildren() {
+        return children;
+    }
+
+    @ElementField(name = "value")
+    public T getValue() {
+        return value;
+    }
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        Tree<String> t = new Tree<String>("top", new Tree[]{new Tree("sub1"), new Tree("sub2")});
+        Saver s = new Saver();
+        String r = s.save(t);
+        System.out.println(r);
+    }
+}
